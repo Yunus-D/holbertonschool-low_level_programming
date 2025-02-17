74d44a9f0e5911617a326d2b8ef2cb4552d2cd7f
MAC <mac@Host-003.lan>
2025-02-17 20:55:16 +0100

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a specific message using printf.
+ *
+ * Description: This function prints exactly the phrase:
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line, using the printf function.
+ * It then returns 0 to indicate successful execution.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
