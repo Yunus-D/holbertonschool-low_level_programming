a0760f5bffe7c88d6302ece7c466364b1d3f47d1
MAC <mac@Host-003.lan>
2025-02-17 20:49:01 +0100

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Affiche un message sur la sortie standard.
+ *
+ * Description: Affiche exactement la phrase:
+ * "Programming is like building a multilingual puzzle"
+ * suivie d'un saut de ligne, en utilisant la fonction puts.
+ *
+ * Return: 0 en cas de succès.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
