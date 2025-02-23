5cd04d483bb7ee0e82f9135e78eb41102279f7d1
MAC <mac@Host-002.lan>
2025-02-24 00:41:57 +0100

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,11 +3,10 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  *
- * Description: Prints the 9 times table formatted as follows:
- *	0,  0,  0,  0,  0,  0,  0,  0,  0,  0
- *	0,  1,  2,  3,  4,  5,  6,  7,  8,  9
- *	0,  2,  4,  6,  8, 10, 12, 14, 16, 18
- *	0,  3,  6,  9, 12, 15, 18, 21, 24, 27
+ * Description:
+ *	Prints the 9 times table in a formatted way, from 0 to 9.
+ *	Each value is separated by a comma and a space.
+ *	The first number on each line is printed without a leading space.
  */
 void	times_table(void)
 {
@@ -20,20 +19,32 @@ void	times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			product = i * j;
-			if (j > 0)
+			if (j == 0)
 			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
+				/* Pour la première colonne, on n'affiche pas de virgule ni d'espace */
+				if (product < 10)
+					_putchar(product + '0');
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 			}
 			else
 			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				/* Pour les colonnes suivantes, on ajoute un espace si le produit est un chiffre */
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 			}
 		}
 		_putchar('\n');
