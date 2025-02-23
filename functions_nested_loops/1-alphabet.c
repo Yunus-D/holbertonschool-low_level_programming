f987254ee7f76f65fba7e04e0b0f238207d47123
MAC <mac@Host-002.lan>
2025-02-23 22:16:55 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *
+ * Description: Uses a for loop to print characters 'a' through 'z',
+ * then prints a newline character. `_putchar` is used exactly twice in the code.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	/* 1er appel à _putchar (dans la boucle for) */
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	/* 2e appel à _putchar pour le saut de ligne */
+	_putchar('\n');
+}
