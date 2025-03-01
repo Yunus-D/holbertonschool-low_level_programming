4e3b5ee857a3270f2caf21d8ddc7b3acfe4e7f03
MAC <mac@Host-003.lan>
2025-03-01 15:10:01 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Description:
+ *	Cette fonction calcule simplement le produit de 'a' et 'b'.
+ *	Aucune fonction de la bibliothèque standard n'est utilisée.
+ *
+ * Constraints:
+ *	- Respect du style Betty
+ *	- Pas de variable globale
+ *	- Prototype: int mul(int a, int b);
+ *
+ * Return: The product of a and b
+ */
+int	mul(int a, int b)
+{
+	return (a * b);
+}
